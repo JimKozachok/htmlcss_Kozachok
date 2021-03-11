c93c79018164ae460c119a2ffc7be4132e1d0991
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-11 15:31:21 +0200

    ю

---
+++ JS/lesson1/cw/js/script1.js
@@ -0,0 +1,8 @@
+alert("Hello world!");
+var name = "Hello world!";
+console.log(name);
+
+var msg = "Hello world";
+alert(msg);
+console.log("variable msg -> " + msg);
+
---
+++ JS/lesson1/cw/js/script2.js
@@ -0,0 +1,4 @@
+var planetName = "Earth"
+console.log('Name of the planet = '+ planetName);
+
+var answer = confirm("Do you live on the Earth?");
