6d22c7780c21c9d1b5a9c28772429f9b2a6aa36e
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-13 13:36:20 +0200

    .

--- JS/lesson1/hw/js/script4.js
+++ JS/lesson1/hw/js/script4.js
@@ -1,11 +1,11 @@
-var name = prompt ("Введите ваше имя: ")
-console.log ("name = " + name);
+var name = prompt("Введите ваше имя: ")
+console.log("name = " + name);
 
-var surname = prompt ("Введите вашу фамилию")
-console.log ("surname = " + surname);
+var surname = prompt("Введите вашу фамилию")
+console.log("surname = " + surname);
 
-var age = prompt ("Введите ваш возраст")
-console.log ("age = " + age);
+var age = prompt("Введите ваш возраст")
+console.log("age = " + age);
 
 document.write("Ваше имя: " + name + '<br/>');
 document.write("Ваша фамилия: " + surname + '<br/>');
