c4a6dd059d9bfd0dcef4a7ec546313e61cf5c858
JimKozachok <78203122+JimKozachok@users.noreply.github.com>
2021-03-13 13:40:28 +0200

    ок

--- JS/lesson1/hw/js/script5.js
+++ JS/lesson1/hw/js/script5.js
@@ -1,13 +1,6 @@
   
 var question = prompt("Введите ваш возраст");
 
-console.log ("age = " + question);
-
 var answer = confirm("Вам точно " +  question + "лет?");
 
-if (answer == true){
-    alert("true");
-    
-    } else { 
-    alert("false");
-    }
+alert(answer);
